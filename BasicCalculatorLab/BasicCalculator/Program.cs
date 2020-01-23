895d9f28b80b62e3b56b0bf5fd6408d88e89c4cf
T-romer <59981890+T-romer@users.noreply.github.com>
2020-01-23 16:44:12 -0600

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -9,16 +9,14 @@ namespace BasicCalculator
             Console.WriteLine("*** BASIC CALCULATOR ***");
 
             Console.WriteLine("Enter the first number");
-
-            // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter the second number");
             int secondNumber = int.Parse(Console.ReadLine());
-
-            int sum = firstNumber + secondNumber;
-
-            Console.WriteLine("The answer is " + sum);
+            Console.WriteLine("Enter the second number");
+            // int.Parse will take a string data type and convert it to an int data type
+            var firstNumber = int.Parse(Console.ReadLine());
+            int v = firstNumber * secondNumber;
+            int product = v;
+            Console.WriteLine("The answer is " + product);
+            Console.WriteLine("Good job!");
         }
     }
 }
